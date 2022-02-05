bfaed3bec4b71dba287ee1f1a4dc917ae0686639
Bobur Yusupov <boburinfo2006@gmail.com>
2022-02-05 22:23:30 +0500

    Project Completed

--- js/foodhub.js
+++ js/foodhub.js
@@ -1,6 +1,22 @@
+const hamburger = document.querySelector('.hamburger');
+const navItems = document.querySelector('.nav-items');
 const nav = document.querySelector('#nav');
 
 //FoodHub
+//Function for Toggling Classes in this project
+function toggleClass(element, className, existingClass) {
+	if(element.classList.contains(className)) {
+		element.classList.remove(className);
+		element.classList.add(existingClass);
+	}else{
+		element.classList.add(className);
+		element.classList.remove(existingClass);
+	}
+}
+
+hamburger.onclick = function () {
+	toggleClass(navItems, 'nav-items-active', 'nav-items');
+}
 
 window.addEventListener("scroll", function(e){
 	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
